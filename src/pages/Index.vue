058f866caf7d759272d8fd3c85db24f1534e3b58
skiboorg <d@skib.org>
2021-09-16 19:52:39 +0300

    fixes

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -14,10 +14,9 @@
       swipeable
       infinite
     >
-      <q-carousel-slide :name="1" img-src="https://cdn.quasar.dev/img/mountains.jpg" />
-      <q-carousel-slide :name="2" img-src="https://cdn.quasar.dev/img/parallax1.jpg" />
-      <q-carousel-slide :name="3" img-src="https://cdn.quasar.dev/img/parallax2.jpg" />
-      <q-carousel-slide :name="4" img-src="https://cdn.quasar.dev/img/quasar.jpg" />
+      <q-carousel-slide :name="1" img-src="~assets/b1.jpg" />
+      <q-carousel-slide :name="2" img-src="~assets/b2.jpg" />
+
     </q-carousel>
 
       </div>
